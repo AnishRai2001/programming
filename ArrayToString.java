15fe1fa1703904f080190edc421b3c0e47450333
Anish Rai <anishrai2023ee@gmail.com>
2025-10-06 23:23:07 +0530

    pm1

---
+++ ArrayToString.java
@@ -0,0 +1,11 @@
+package popular;
+
+public class ArrayToString {
+	public static void main(String[] args) {
+		char[]arr= {'h','e','l','l','o'};
+		String ans=new String(arr);
+		System.out.println(ans);
+		
+	}
+
+}
