cf791436bcb06ea25bffab62963606c1150386cc
Anish Rai <anishrai2023ee@gmail.com>
2025-10-14 23:54:33 +0530

    done

---
+++ String_palindrome.java
@@ -0,0 +1,24 @@
+package String;
+
+public class String_palindrome {
+    public static void main(String[] args) {
+        String str = "ani";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            rev = rev + str.charAt(i);
+        }
+            if (str.equals(rev))
+            {
+                System.out.println("Palindrome");
+            }
+            else
+            {
+                System.out.println("Not Palindrome");
+
+            }
+
+
+    }
+}
+
